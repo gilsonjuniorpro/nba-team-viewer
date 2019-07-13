822d0a58a383d5bc6468c373fcb4f0b021b24e0f
Gilson <gilsonjuniorpro@gmail.com>
2019-07-13 02:56:23 -0400

    added espresso tests

--- NBA/app/src/androidTest/java/com/nba/ca/ExampleInstrumentedTest.kt
+++ NBA/app/src/androidTest/java/com/nba/ca/ExampleInstrumentedTest.kt
@@ -1,12 +1,33 @@
 package com.nba.ca
 
+import android.content.ClipData
+import android.view.View
 import androidx.test.InstrumentationRegistry
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.NoMatchingViewException
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeLeft
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions.scrollTo
+import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
+import com.nba.ca.controller.TeamController
+import com.nba.ca.pojo.ResponseTeams
+import com.nba.ca.ui.MainActivity
+import com.nba.ca.util.Dominios
+import kotlinx.android.synthetic.main.activity_main.*
+import org.hamcrest.Matchers.*
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +36,56 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    val rule : ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("com.nba.ca", appContext.packageName)
     }
+
+    @Test
+    fun verifyIfListTeamsLoads(){
+        var response = TeamController.listTeams() as ResponseTeams
+        assertEquals(response.teams!!.size, Dominios.AMOUNT_OF_TEAMS)
+    }
+
+    @Test
+    fun verifyIfListPlayerLoads(){
+        var response = TeamController.listTeams() as ResponseTeams
+        assertEquals(response.teams!![0].players!!.size, Dominios.AMOUNT_OF_PLAYERS)
+    }
+
+    @Test
+    fun simulateClickOnSortAsc(){
+        onView(withId(R.id.ivSortAsc)).perform(click())
+    }
+
+    @Test
+    fun simulateClickOnSortDesc(){
+        onView(withId(R.id.ivSortDesc)).perform(click())
+    }
+
+    @Test
+    fun simulateClickOnSortWins(){
+        onView(withId(R.id.ivSortWins)).perform(click())
+    }
+
+    @Test
+    fun simulateClickOnSortLosses(){
+        onView(withId(R.id.ivSortLosses)).perform(click())
+    }
+
+    @Test
+    fun simulateClickOnItem(){
+        onView(withId(R.id.listTeams))
+            .perform(swipeLeft())
+            .check(matches(hasDescendant(withText("Atlanta Hawks"))))
+    }
+
+
 }
--- NBA/app/src/main/java/com/nba/ca/util/Dominios.kt
+++ NBA/app/src/main/java/com/nba/ca/util/Dominios.kt
@@ -6,6 +6,8 @@ class Dominios {
         const val URL_JSON_NBA = "https://raw.githubusercontent.com/scoremedia/nba-team-viewer/master/input.json"
         const val URL_JSON_LOGO = "http://www.projectconnect.com.br/nba-php/api/logo/list.php"
         const val URL_JSON_LOGO_IMAGE = "http://www.projectconnect.com.br/nba-php/images/"
+        const val AMOUNT_OF_TEAMS = 30
+        const val AMOUNT_OF_PLAYERS = 16
     }
 
 }
